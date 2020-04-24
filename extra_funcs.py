cdceee0af72a660e831670986de746a6d6179a31
ChristianMichelsen <christianmichelsen@gmail.com>
2020-04-24 10:44:23 +0200

    Fix title for figures
    Former-commit-id: 459d9be0b2886b2f97ca62e9c58e9242b4bbe5cc
    Former-commit-id: f282031ee3b1a8331c34633c4cb9756dabd60b86

--- extra_funcs.py
+++ extra_funcs.py
@@ -227,3 +227,12 @@ def uniform(a, b):
     loc = a
     scale = b-a
     return sp_uniform(loc, scale)
+
+
+def human_format(num):
+    num = float('{:.3g}'.format(num))
+    magnitude = 0
+    while abs(num) >= 1000:
+        magnitude += 1
+        num /= 1000.0
+    return '{}{}'.format('{:f}'.format(num).rstrip('0').rstrip('.'), ['', 'K', 'M', 'B', 'T'][magnitude])
