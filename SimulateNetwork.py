7cd05f9cca4af9baea4e7a6c93116dfa48204918
ChristianMichelsen <christianmichelsen@gmail.com>
2020-04-30 21:35:06 +0200

    gamma sigma
    Former-commit-id: d48b809ce08663f94d426060031728dc00677be1
    Former-commit-id: 198e1990c6c46e4718843d8c47b85d64b5632df6
    Former-commit-id: 5f9f62b8e0aa671f79db8abd0dc0cf1b0da2d9d4
    Former-commit-id: 45dcdcff5874a18a0a2bd04517e65f4c3a66abaa
    Former-commit-id: 7009c66423dc518a2c7cd749b3a84ce32a75af66

--- SimulateNetwork.py
+++ SimulateNetwork.py
@@ -5,7 +5,7 @@ import SimulateNetwork_extra_funcs as extra_funcs
 from pathlib import Path
 from importlib import reload
 
-num_cores_max = 20
+num_cores_max = 38
 
 def is_local_computer(N_local_cores=8):
     import platform
@@ -31,17 +31,17 @@ def generate_filenames(N_loops=10, force_overwrite=False):
                     Nstates = 9,
                 )
 
-    # for gamma in [0.15, 0.3, 0.45]:
-    #     dict_in['gamma'] = gamma
+    for gamma in [0.15, 0.3, 0.45]:
+        dict_in['gamma'] = gamma
 
-    #     for sigma in [0.15, 0.3, 0.45]:
-    #         dict_in['sigma'] = sigma
+        for sigma in [0.15, 0.3, 0.45]:
+            dict_in['sigma'] = sigma
 
-    for alpha in [1, 2, 4, 8]:
-        dict_in['alpha'] = alpha
+    #for alpha in [1, 2, 4, 8]:
+    #    dict_in['alpha'] = alpha
 
-        for psi in [0, 1, 4]:
-            dict_in['psi'] = psi
+     #   for psi in [0, 1, 4]:
+      #      dict_in['psi'] = psi
 
             for ID in range(N_loops):
                 filename = extra_funcs.dict_to_filename(dict_in, ID)
