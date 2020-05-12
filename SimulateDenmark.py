d6721128f3682a759fa1e591494b515192ae2b26
ChristianMichelsen <christianmichelsen@gmail.com>
2020-05-12 23:53:16 +0200

    clean print messages
    Former-commit-id: 84c5f49142895f3e022a6b1410a5a243446b874f
    Former-commit-id: 10e800de8a9cd109edff63addafa50f0e45e9247
    Former-commit-id: 3e26c28c08e2edb8c735e367919bf43787426543
    Former-commit-id: 2f88d5eda1a3cdb429ce4e0b0843e330c4be9d8b
    Former-commit-id: 647af834e193f9bceafb4c8bd561ba1f1be637ec [formerly 42c2374fc3dd24dec7835a19ffce7a7c1260fadf]
    Former-commit-id: 45832bb120711e0f6bf89e2b6eb7c9d61b1ac3e2

--- SimulateDenmark.py
+++ SimulateDenmark.py
@@ -5,7 +5,7 @@ import SimulateDenmark_extra_funcs as extra_funcs
 from pathlib import Path
 from importlib import reload
 
-num_cores_max = 7
+num_cores_max = 8
 N_loops = 100
 N_Denmark = extra_funcs.N_Denmark # 535_806
 force_SK_P1_UK = True
@@ -67,18 +67,17 @@ all_sim_pars = [
 def get_filenames_iter(all_sim_pars, force_SK_P1_UK, N_loops):
     all_filenames = []
     if not force_SK_P1_UK:
+        d_sim_pars = []
         for d_simulation_parameters in all_sim_pars:
             filenames = extra_funcs.generate_filenames(d_simulation_parameters, N_loops, force_SK_P1_UK=force_SK_P1_UK)
             all_filenames.append(filenames)
+            d_sim_pars.append(d_simulation_parameters)
     else:
         for d_simulation_parameters in all_sim_pars:
             all_filenames.extend(extra_funcs.generate_filenames(d_simulation_parameters, N_loops, force_SK_P1_UK=force_SK_P1_UK))
         all_filenames = [all_filenames]
-    return all_filenames
-
-# all_filenames = get_filenames_iter(all_sim_pars, False, N_loops)
-# len(all_filenames)
-# len(all_filenames[0])
+        d_sim_pars = ['all configurations']
+    return all_filenames, d_sim_pars
 
 # reload(extra_funcs)
 if __name__ == '__main__':
@@ -88,19 +87,19 @@ if __name__ == '__main__':
 
     all_filenames = get_filenames_iter(all_sim_pars, force_SK_P1_UK, N_loops)
 
-    for filenames in all_filenames:
-        # filenames = extra_funcs.generate_filenames(d_simulation_parameters, N_loops, force_SK_P1_UK=force_SK_P1_UK)
+    for filenames, d_simulation_parameters in zip(*all_filenames):
         N_files = len(filenames)
+        # filenames = extra_funcs.generate_filenames(d_simulation_parameters, N_loops, force_SK_P1_UK=force_SK_P1_UK)
         # filename = filenames[0]
 
-        # print(d_simulation_parameters, N_files)
+        print(d_simulation_parameters, N_files)
         # continue
 
         # make sure path exists
         if len(filenames) > 0:
             filename = filenames[0]
-            # print(f"Generating {N_files} network-based simulations with {num_cores} cores based on {d_simulation_parameters}, please wait.", flush=True)
-            print(f"Generating {N_files} network-based simulations with {num_cores} cores, please wait.", flush=True)
+            print(f"Generating {N_files} network-based simulations with {num_cores} cores based on {d_simulation_parameters}, please wait.", flush=True)
+            # print(f"Generating {N_files} network-based simulations with {num_cores} cores, please wait.", flush=True)
 
             if num_cores == 1:
                 for filename in tqdm(filenames):
@@ -111,28 +110,6 @@ if __name__ == '__main__':
                     list(tqdm(p.imap_unordered(extra_funcs.single_run_and_save, filenames), total=N_files))
         else:
             print("No files to generate, everything already generated.")
-
-    # else:
-
-    #     filenames = []
-    #     for d_simulation_parameters in all_sim_pars:
-    #         filenames.extend(extra_funcs.generate_filenames(d_simulation_parameters, N_loops, force_SK_P1_UK=force_SK_P1_UK))
-    #     N_files = len(filenames)
-
-    #     # make sure path exists
-    #     if len(filenames) > 0:
-    #         filename = filenames[0]
-    #         print(f"Generating {N_files} network-based simulations with {num_cores} cores based on {d_simulation_parameters}, please wait.", flush=True)
-
-    #         if num_cores == 1:
-    #             for filename in tqdm(filenames):
-    #                 extra_funcs.single_run_and_save(filename)
-
-    #         else:
-    #             with mp.Pool(num_cores) as p:
-    #                 list(tqdm(p.imap_unordered(extra_funcs.single_run_and_save, filenames), total=N_files))
-    #     else:
-    #         print("No files to generate, everything already generated.")
 
     print("Finished simulating!")
 
