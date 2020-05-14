732000cb7f3390d96faa344ae67aa914f7dac95f
ChristianMichelsen <christianmichelsen@gmail.com>
2020-05-14 17:18:42 +0200

    Add animation v0.3
    Former-commit-id: 4c7c7cd508dc9a68afa6822117321e19ddfef190
    Former-commit-id: 1651a706ba8477497dec5c7e3ddb6eaf1e00038b
    Former-commit-id: 518d7d408561e90828f2f43e983f2d5547aa1f5f
    Former-commit-id: efc707a249f6a0bf3c880f6f6f0c92d823d3280e
    Former-commit-id: 4e9aff7ac1c0ab5ac9e6f2b909cf3753b57f5568 [formerly acfdf1b97ed39a128b86d2a9e6c60e1fa1e908fd]
    Former-commit-id: 1a0e373e36f903ef68f568ed34a4f5410de4d1d0

--- SK_P1_UK_analysis.py
+++ SK_P1_UK_analysis.py
@@ -32,13 +32,26 @@ from astropy.visualization import LogStretch
 from astropy.visualization.mpl_normalize import ImageNormalize
 import matplotlib.patches as mpatches
 from matplotlib.lines import Line2D
-from matplotlib.ticker import EngFormatter
+from matplotlib.ticker import EngFormatter, PercentFormatter, MaxNLocator
+from scipy.interpolate import interp1d
+import matplotlib as mpl
 
 #%%
 import shutil
-# import imageio # conda install imageio
 import subprocess
 import warnings
+
+
+def add_spines(ax, exclude=None):
+    if exclude is None:
+        exclude = []
+    spines = ['left', 'bottom']
+    for spine in spines:
+        if not spine in exclude:
+            ax.spines[spine].set_color('k')
+            ax.spines[spine].set_linewidth(2)
+    ax.tick_params(axis='x', pad=10)
+
 
 from copy import copy
 class AnimateSIR:
@@ -65,6 +78,7 @@ class AnimateSIR:
         self._init_plot()
         self.df_counts = df_counts if df_counts is not None else self._compute_df_counts()
         self.R_t = self._calc_infection_rate_R_t(**R_t_kwargs)
+        self.N0 = self.df_counts.iloc[0].sum()
 
     def _get_df(self, i_day):
         df = pd.DataFrame(self.P1, columns=['x', 'y'])
@@ -82,13 +96,19 @@ class AnimateSIR:
         self.colors = ['#7F7F7F', '#D62728', '#2CA02C']
         self.d_colors = {'S': '#7F7F7F', 'I': '#D62728', 'R': '#2CA02C'}
         
-
         self.norm_1000 = ImageNormalize(vmin=0., vmax=1000, stretch=LogStretch())
         self.norm_100 = ImageNormalize(vmin=0., vmax=100, stretch=LogStretch())
         self.norm_10 = ImageNormalize(vmin=0., vmax=10, stretch=LogStretch())
 
         # self.states = ['S', 'E', 'I', 'R']
         self.states = ['S', 'I', 'R']
+        self.state_names = {'S': 'Susceptable', 'I': 'Infected', 'R': 'Recovered'}
+
+        # create the new map
+        cmap = mpl.colors.ListedColormap([self.d_colors['R'], self.d_colors['I']])
+        bounds = [0, 0.5, 1]
+        norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+        self._scatter_kwargs = dict(cmap=cmap, norm=norm, edgecolor='none')
         return None
 
     def _compute_df_counts(self):
@@ -102,6 +122,16 @@ class AnimateSIR:
             counts_i_day[i_day] = {key: len(val) for key, val in dfs.items()}
         df_counts = pd.DataFrame(counts_i_day).T
         return df_counts
+
+    def _interpolate_R_t(self, R_t):
+        N = len(R_t)
+        x = np.arange(N)
+        y = R_t 
+        f = interp1d(x, y, bounds_error=False, fill_value="extrapolate")
+        x_interpolated = np.linspace(0, N-1, 1000)
+        y_interpolated = f(x_interpolated)
+        df_R_t = pd.DataFrame({'t': x_interpolated, 'R_t': y_interpolated})
+        return df_R_t
 
     def _calc_infection_rate_R_t(self, time_delay=3):
         df_counts = self.df_counts
@@ -126,7 +156,7 @@ class AnimateSIR:
         dfs = {s: df.query("SK == @s") for s in self.states}
 
         # Main plot
-        k_scale = 1.5
+        k_scale = 1.8
         fig = plt.figure(figsize=(10*k_scale, 13*k_scale))
         ax = fig.add_subplot(1, 1, 1, projection='scatter_density')
 
@@ -134,61 +164,61 @@ class AnimateSIR:
             ax.scatter_density(dfs['S']['x'], dfs['S']['y'], color=self.d_colors['S'], alpha=0.2, norm=self.norm_1000, dpi=dpi)
         if len(dfs['R']) > 0:
             ax.scatter_density(dfs['R']['x'], dfs['R']['y'], color=self.d_colors['R'], alpha=0.3, norm=self.norm_100, dpi=dpi)
-        # if len(dfs['E']) > 0:
-            # ax.scatter_density(dfs['E']['x'], dfs['E']['y'], color=self.colors[1], norm=self.norm_10, dpi=dpi)
         if len(dfs['I']) > 0:
             ax.scatter_density(dfs['I']['x'], dfs['I']['y'], color=self.d_colors['I'], norm=self.norm_10, dpi=dpi)
         ax.set(xlim=(8, 13.7), ylim=(54.52, 58.2))
 
         kw_args_circle = dict(xdata=[0], ydata=[0], marker='o', color='w', markersize=16)
-        circles = [Line2D(label=state, markerfacecolor=color, **kw_args_circle) for color, state in zip(self.colors, self.states)]
+        circles = [Line2D(label=self.state_names[state], markerfacecolor=self.d_colors[state], **kw_args_circle) for state in self.states]
         ax.legend(handles=circles, loc='upper left', fontsize=20)
 
         cfg = extra_funcs.filename_to_dotdict(self.filename, SK_P1_UK=True)
         title = extra_funcs.dict_to_title(cfg)
-        ax.set_title(title, pad=50, fontsize=16)
+        ax.set_title(title, pad=50, fontsize=19)
 
         # secondary plots:
 
         # These are in unitless percentages of the figure size. (0,0 is bottom left)
-        left, bottom, width, height = [0.6, 0.76, 0.3, 0.08]
+        left, bottom, width, height = [0.62, 0.76, 0.3*0.95, 0.08*0.9]
 
-        if True:
-            background_box = [(0.5, 0.6), 0.47, 0.385]
-            ax.add_patch(mpatches.Rectangle(*background_box, facecolor='white', edgecolor='lightgray', transform=ax.transAxes))
-
+        background_box = [(0.51, 0.61), 0.47, 0.36]
+        ax.add_patch(mpatches.Rectangle(*background_box, facecolor='white', edgecolor='lightgray', transform=ax.transAxes))
 
         i_day_max = i_day + max(3, i_day*0.1)
 
-        delta_width = 0 * width / 100
-        ax2 = fig.add_axes([left+delta_width, bottom, width-delta_width, height])
-        I_up_to_today = self.df_counts['I'].iloc[:i_day+1]
+        # delta_width = 0 * width / 100
+        ax2 = fig.add_axes([left, bottom, width, height])
+        I_up_to_today = self.df_counts['I'].iloc[:i_day+1] / self.N0
         ax2.plot(I_up_to_today.index, I_up_to_today, '-', color=self.d_colors['I'])
         ax2.plot(I_up_to_today.index[-1], I_up_to_today.iloc[-1], 'o', color=self.d_colors['I'])
         I_max = np.max(I_up_to_today)
-        ax2.set(xlabel='t', ylabel='I', ylim=(0, I_max*1.2), xlim=(0, i_day_max))
-        ax2.yaxis.set_major_formatter(EngFormatter())
-        # ax2.hist(df['UK_num'], range=(0, self.UK_max), bins=self.UK_max, color=self.colors[0])
+        ax2.set(xlabel='t', ylim=(0, I_max*1.2), xlim=(0, i_day_max))
+        decimals = max(int(-np.log10(I_max)) - 1, 0) # max important, otherwise decimals=-1
+        ax2.yaxis.set_major_formatter(PercentFormatter(xmax=1, decimals=decimals))
+        ax2.xaxis.set_major_locator(MaxNLocator(integer=True))
+        ax2.text(-0.28, 0.2, 'Infected', fontsize=20, transform=ax2.transAxes, rotation=90)
+        add_spines(ax2)
 
-
-        ax3 = fig.add_axes([left, bottom-height*1.6, width, height])
-        R_t_up_to_today = self.R_t[:i_day+1]
-        ax3.plot(range(i_day+1), R_t_up_to_today, '-', color=self.d_colors['S'])
-        ax3.plot(i_day, R_t_up_to_today[-1], 'o', color=self.d_colors['S'])
-        R_t_max = np.nanmax(R_t_up_to_today)
-        if np.isnan(R_t_max):
-            R_t_max = 4
-        ax3.set(xlabel='t', ylabel='R_t', ylim=(0, R_t_max), xlim=(0, i_day_max))
-        # x, y = np.unique(df['SK_num'], return_counts=True)
-        # ax3.bar(x, y, color=self.colors[0])
-        # ax3.xaxis.set_ticks(np.arange(-1, 9))
-        # ax3.set(xlabel='State', ylabel='Counts')
-        # ax3.set_yscale('log')
+        ax3 = fig.add_axes([left, bottom-height*1.8, width, height])
+        if i_day > 0:
+            R_t_up_to_today = self._interpolate_R_t(self.R_t[:i_day+1])
+            z = (R_t_up_to_today['R_t'] > 1) / 1
+            ax3.scatter(R_t_up_to_today['t'], R_t_up_to_today['R_t'], s=10, c=z, **self._scatter_kwargs)
+            R_t_today = R_t_up_to_today.iloc[-1]
+            z_today = (R_t_today['R_t'] > 1)
+            ax3.scatter(R_t_today['t'], R_t_today['R_t'], s=100, c=z_today, **self._scatter_kwargs)
+        R_t_max = 3
+        ax3.axhline(1, ls='--', color='k', lw=1) # x = 0
+        # ax3.axhline(0, color='k', lw=2) # x = 0
+        ax3.set(xlabel='t', ylim=(0, R_t_max), xlim=(0, i_day_max))
+        ax3.xaxis.set_major_locator(MaxNLocator(integer=True))
+        ax3.text(-0.25, 0.4, r'$\mathregular{R_\infty}$', fontsize=24, transform=ax3.transAxes, rotation=90)
+        add_spines(ax3)
 
         ax.text(0.02, 0.02, f"Day: {i_day}", fontsize=24, transform=ax.transAxes)
 
         plt.close('all')
-        return fig, ax
+        return fig, (ax, ax2, ax3)
 
 
     def make_animation(self, dpi=50, remove_frames=True, force_rerun=False, optimize_gif=True):
@@ -229,7 +259,7 @@ class AnimateSIR:
         if do_tqdm:
             it = tqdm(it, desc='Make individual frames')
         for i_day in it:
-            fig, ax = self._plot_df_i_day(i_day, dpi)
+            fig, _ = self._plot_df_i_day(i_day, dpi)
             png_name = self._get_png_name(i_day)
             Path(png_name).parent.mkdir(parents=True, exist_ok=True)
             fig.savefig(png_name, dpi=dpi, bbox_inches='tight', pad_inches=0.3)
@@ -290,16 +320,9 @@ num_cores = get_num_cores(num_cores_max)
 
 filenames = get_SK_P1_UK_filenames()
 filename = filenames[1]
-N_files = len(filenames)                
+N_files = len(filenames)           
 
 # animate_file(filename, do_tqdm=True, verbose=True, force_rerun=True)
-
-# # df_counts = animation.df_counts
-# df_counts = None
-# i_day = 0
-# animation = AnimateSIR(filename, verbose=True, do_tqdm=True, df_counts=df_counts)
-# fig, ax = animation._plot_df_i_day(i_day, dpi=50)
-# fig
 
 #%%
 
@@ -307,7 +330,10 @@ N_files = len(filenames)
 # for filename in tqdm(filenames):
 #     animate_file(filename, do_tqdm=True, verbose=True, force_rerun=True)
 
-# if __name__ == '__main__':
-#     print(f"Generating frames using {num_cores} cores, please wait", flush=True)
-#     with mp.Pool(num_cores) as p:
-#         list(tqdm(p.imap_unordered(animate_file, filenames), total=N_files))
+if __name__ == '__main__':
+    print(f"Generating frames using {num_cores} cores, please wait", flush=True)
+    with mp.Pool(num_cores) as p:
+        list(tqdm(p.imap_unordered(animate_file, filenames), total=N_files))
+
+
+# %%
