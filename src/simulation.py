6bf593ebc6fbc7863c90d6be6632a9929d9ed450
ChristianMichelsen <christianmichelsen@gmail.com>
2020-08-10 22:31:45 +0200

    comment some code out

--- src/simulation.py
+++ src/simulation.py
@@ -876,23 +876,25 @@ def run_full_simulation(filename, verbose=False, force_rerun=False, only_initial
     if verbose:
         print("\n\nFinished!!!")
 
-reload(utils)
-reload(simulation_utils)
 
-verbose = True
-force_rerun = False
-filename = 'Data/ABN/N_tot__58000__N_init__100__N_ages__1__mu__40.0__sigma_mu__0.0__beta__0.01__sigma_beta__0.0__rho__0.0__lambda_E__1.0__lambda_I__1.0__epsilon_rho__0.01__beta_scaling__1.0__age_mixing__1.0__algo__2/N_tot__58000__N_init__100__N_ages__1__mu__40.0__sigma_mu__0.0__beta__0.01__sigma_beta__0.0__rho__0.0__lambda_E__1.0__lambda_I__1.0__epsilon_rho__0.01__beta_scaling__1.0__age_mixing__1.0__algo__2__ID__000.csv'
-# filename = filename.replace('58000', '580000')
-filename = filename.replace('ID__000', 'ID__002')
-filename = filename.replace('rho__0.0__', 'rho__25.0__')
 
-if True:
-    simulation = Simulation(filename, verbose)
-    simulation.initialize_network(force_rerun=force_rerun)
-    simulation.make_initial_infections()
-    simulation.run_simulation()
-    df = simulation.make_dataframe()
-    display(df)
+# reload(utils)
+# reload(simulation_utils)
+
+# verbose = True
+# force_rerun = False
+# filename = 'Data/ABN/N_tot__58000__N_init__100__N_ages__1__mu__40.0__sigma_mu__0.0__beta__0.01__sigma_beta__0.0__rho__0.0__lambda_E__1.0__lambda_I__1.0__epsilon_rho__0.01__beta_scaling__1.0__age_mixing__1.0__algo__2/N_tot__58000__N_init__100__N_ages__1__mu__40.0__sigma_mu__0.0__beta__0.01__sigma_beta__0.0__rho__0.0__lambda_E__1.0__lambda_I__1.0__epsilon_rho__0.01__beta_scaling__1.0__age_mixing__1.0__algo__2__ID__000.csv'
+# # filename = filename.replace('58000', '580000')
+# filename = filename.replace('ID__000', 'ID__002')
+# filename = filename.replace('rho__0.0__', 'rho__25.0__')
+
+# if True:
+#     simulation = Simulation(filename, verbose)
+#     simulation.initialize_network(force_rerun=force_rerun)
+#     simulation.make_initial_infections()
+#     simulation.run_simulation()
+#     df = simulation.make_dataframe()
+#     display(df)
 
 # # if verbose:
 # #     print("\n\n")
