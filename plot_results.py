c04b528a1cc9b030e0cf8d8ae978065a739f15e6
ChristianMichelsen <christianmichelsen@gmail.com>
2020-08-12 10:11:51 +0200

    add higher rho figurer

--- plot_results.py
+++ plot_results.py
@@ -63,8 +63,6 @@ if False:
     plot.plot_1D_scan('epsilon_rho', fit_objects_all, rho=100)
     plot.plot_1D_scan('epsilon_rho', fit_objects_all, rho=100, algo=1)
 
-    # extra_funcs.plot_1D_scan('mu', fit_objects_all)
-    # extra_funcs.plot_1D_scan('beta', fit_objects_all)
     plot.plot_1D_scan('sigma_beta', fit_objects_all)
     plot.plot_1D_scan('sigma_beta', fit_objects_all, algo=1)
     plot.plot_1D_scan('sigma_beta', fit_objects_all, rho=100)
