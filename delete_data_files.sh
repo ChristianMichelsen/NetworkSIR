7b581f0121c32c60a29fcfcc5749fd607c534c82
Christian Michelsen <christianmichelsen@gmail.com>
2020-11-03 15:36:52 +0100

    Add file deleter

---
+++ delete_data_files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+read -r -p "Going to delete datafiles, are you sure? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    rm -rf Data/network &; rm -rf Data/ABM &; rm -rf Data/cfgs &;
+else
+    echo "Not deleting anything"
+fi
+
+read -r -p "Also delete initialized networks? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    rm -rf Data/initialized_network
+else
+    echo "Not deleting anything"
+fi
